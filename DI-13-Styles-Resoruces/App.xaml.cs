e70f0215e64e4c75d3326ff217d0b8defb67fd78
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2017-01-10 11:59:30 +0100

    Resources Done

--- DI-13-Styles-Resoruces/App.xaml.cs
+++ DI-13-Styles-Resoruces/App.xaml.cs
@@ -40,10 +40,10 @@ namespace DI_13_Styles_Resoruces
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
+            /*if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
-            }
+            }*/
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
 
